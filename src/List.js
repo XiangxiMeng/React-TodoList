410069f3b2e8918ad4b1354d3bd7ddb49f44da3d
Xiangxi MENG <xiangxim@icloud.com>
2020-04-17 19:50:51 +0800

    Change the way state updated

--- src/List.js
+++ src/List.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ListItem from './ListItem';
 
-export class TodoList extends React.Component {
+class List extends React.Component {
   render(){
     var items = this.props.items.map(
       (item, index) => {
@@ -13,27 +13,11 @@ export class TodoList extends React.Component {
     // console.log(items)
     return(
       <div>
-        <h1>Scheduled</h1>
+        <h1> {this.props.name} </h1>
         <ul> {items} </ul>
       </div> 
     );
   }
 }
 
-export class DoneList extends React.Component {
-  render(){
-    var items = this.props.items.map(
-      (item, index) => {
-        return (
-          <ListItem key={index} item={item} index={index} removeItem={this.props.removeItem} markItem={this.props.markItem} />
-        );
-      }
-    )
-    return(
-      <div>
-        <h1>Completed</h1>
-        <ul> {items} </ul>
-      </div>
-    );
-  }
-}
+export default List;
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import TodoForm from './TodoForm';
-import {TodoList, DoneList} from './List';
-
-// Initialize with some examples...
-var todoItems = [];
-todoItems.push({index: 1, value: "Work out", done: false});
-todoItems.push({index: 2, value: "Learn react", done: false});
-todoItems.push({index: 3, value: "Thesis", done: false});
-var doneItems = [];
-doneItems.push({index: 4, value: "Shopping", done: true});
-doneItems.push({index: 5, value: "Attendance", done: true});
-doneItems.push({index: 6, value: "Quiz", done: true});
-
+import List from './List';
 
 class TodoApp extends React.Component {
   constructor(props){
@@ -21,42 +10,42 @@ class TodoApp extends React.Component {
     this.removeItem = this.removeItem.bind(this);
     this.state = {
       // Update two lists, todo and done
-      todoItems: todoItems,
-      doneItems: doneItems
+      todoItems: [{value: "Work out", done: false}, {value: "Learn React", done: false}],
+      doneItems: [{value: "Shopping", done: true}, {value: "Thesis", done:true}]
     }
   }
   addItem(todoItem){
     // deal with onSubmit in TodoForm
-    todoItems.unshift({
-      index: todoItems.length+1,
-      value: todoItem.newItemValue,
-      done: false
-    });
-    this.setState({todoItems: todoItems})
-    console.log(todoItems.length);
+    this.setState(preState => ({
+      todoItems: preState.todoItems.concat([{value: todoItem.newItemValue, done: false}])
+    }));
+    // console.log(todoItems.length);
   }
   removeItem(itemIndex){
     // deal with click on donelist
-    doneItems.splice(itemIndex,1);
-    this.setState({doneItems: doneItems});
-    console.log(itemIndex);
+    this.setState(preState => ({
+      doneItems: preState.doneItems.slice(0,itemIndex).concat(preState.doneItems.slice(itemIndex+1))
+    }));
+    console.log(itemIndex, "Remove");
   }
   markItem(itemIndex){
     // deal with click on todolist
-    var alreadyDone = todoItems[itemIndex];
-    todoItems.splice(itemIndex,1);
+    var alreadyDone = this.state.todoItems[itemIndex];
     alreadyDone.done = true;
-    doneItems.push(alreadyDone);
-    this.setState({todoItems:todoItems, doneItems:doneItems});
+    this.setState(preState => ({
+      todoItems: preState.todoItems.slice(0,itemIndex).concat(preState.todoItems.slice(itemIndex+1)),
+      doneItems: preState.doneItems.concat(alreadyDone)
+    }));
     console.log(itemIndex);
+    console.log(alreadyDone.value);
   }
   render(){
     // Deliver different functions to components by props
     return(
       <div>
         <TodoForm addItem={this.addItem} />
-        <TodoList items={todoItems} removeItem={this.removeItem} markItem={this.markItem} />
-        <DoneList items={doneItems} removeItem={this.removeItem} markItem={this.markItem} />
+        <List name="Todo" items={this.state.todoItems} removeItem={this.removeItem} markItem={this.markItem} />
+        <List name="Done" items={this.state.doneItems} removeItem={this.removeItem} markItem={this.markItem} />
       </div>
     );
   }
